4f8e943b9eb7586b79fcf52469bebad78102ce87
Assaf Mazor <assaf.player.king@gmail.com>
2021-12-21 11:52:31 +0200

    ...

--- home-page/client/main.js
+++ home-page/client/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(window.scroll).scroll(function () {
-        if(window.pageYOffset > 700){
+        if(window.pageYOffset > 600){
             $(".arrow-up-wrap").removeClass("hide")
         }else {
             $(".arrow-up-wrap").addClass("hide")
@@ -9,7 +9,7 @@ $(document).ready(function () {
 })
 function onScrollDownBtnClick(){
     $("html, body").animate({
-        scrollTop: $(".projects-page").offset().top - 10
+        scrollTop: $(".projects-page").offset().top
     }, "slow");
 }
 
